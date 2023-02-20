a094ab52cb87a25263dc44a36621faf782940209
edwinnambaje <nambajeeedwin@gmail.com>
2023-02-21 01:18:06 +0200

    added homepage

---
+++ src/Home/Home.js
@@ -0,0 +1,17 @@
+import './Home.css'
+function Home() {
+    return (
+        <section id="hero">
+        <div className="hero container">
+          <div>
+            <h1>Hello, <span></span></h1>
+            <h1>My Name is <span></span></h1>
+            <h1 className='ed'><span>Edwin</span></h1>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Home;
+  
